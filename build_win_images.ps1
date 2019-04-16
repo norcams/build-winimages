16eb8fef30dfe992595d855bd7c96829003aaffc
TorLdre <tor.ledre@gmail.com>
2019-04-16 14:25:54 +0200

    filter in cumulative updates instead

--- build_win_images.ps1
+++ build_win_images.ps1
@@ -72,7 +72,7 @@ Import-Module "$winimagebuilderpath\WinImageBuilder.psm1"
 
 $windowsVersions | ForEach-Object -Process {
   # Download latest Cumulative Update from Windows Update
-  $updates = $(Get-LatestUpdate -WindowsVersion $_.likeness -Build $_.build | Where-Object { $_.Note -notlike "*Delta*"})
+  $updates = $(Get-LatestUpdate -WindowsVersion $_.likeness -Build $_.build | Where-Object { $_.Note -like "*Cumulative*"})
   $dlversion = $_.version
   $dlupdate = $updates | Where-Object -Property Note -Like "*$dlversion*"
   $filename = $dlupdate.URL.Split('/')[-1]
